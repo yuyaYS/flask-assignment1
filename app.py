5e9105a58a118309124e665cde92ff069de7e05f
Yuya <y303sushi@gmail.com>
2021-01-12 22:51:42 +0900

    change ge_users fucntion

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+# @app.route('/users')
+# def get_users():
+#    return users
+
 @app.route('/users')
 def get_users():
-   return users
-
-@app.route('/users/<id>')
-def get_user(id):
-   if id :
+   search_username = request.args.get('name') #accessing the value of parameter 'name'
+   if search_username :
+      subdict = {'users_list' : []}
       for user in users['users_list']:
-        if user['id'] == id:
-           return user
-      return ({})
+         if user['name'] == search_username:
+            subdict['users_list'].append(user)
+      return subdict
    return users
 
-   
 users = { 
    'users_list' :
    [
