f2a6aa23175fced4f0f9fbc59a61f3c3b70491e0
Yuya <y303sushi@gmail.com>
2021-01-12 22:53:31 +0900

    change get_users to get based on the id

--- app.py
+++ app.py
@@ -10,17 +10,26 @@ def hello_world():
 # def get_users():
 #    return users
 
-@app.route('/users')
-def get_users():
-   search_username = request.args.get('name') #accessing the value of parameter 'name'
-   if search_username :
-      subdict = {'users_list' : []}
-      for user in users['users_list']:
-         if user['name'] == search_username:
-            subdict['users_list'].append(user)
-      return subdict
-   return users
+# @app.route('/users')
+# def get_users():
+#    search_username = request.args.get('name') #accessing the value of parameter 'name'
+#    if search_username :
+#       subdict = {'users_list' : []}
+#       for user in users['users_list']:
+#          if user['name'] == search_username:
+#             subdict['users_list'].append(user)
+#       return subdict
+#    return users
 
+@app.route('/users/<id>')
+def get_user(id):
+   if id :
+      for user in users['users_list']:
+        if user['id'] == id:
+           return user
+      return ({})
+   return users
+   
 users = { 
    'users_list' :
    [
