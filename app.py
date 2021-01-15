c9f82561586083700d9a835c313511cf446aaf45
Yuya <y303sushi@gmail.com>
2021-01-16 04:52:26 +0900

    add filter by nae and job && delete function

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask import request
+from flask import jsonify,make_response
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'it will return the item based on the route!'
 
-# @app.route('/users')
-# def get_users():
-#    return users
+
 
 # @app.route('/users')
 # def get_users():
@@ -21,15 +21,69 @@ def hello_world():
 #       return subdict
 #    return users
 
-@app.route('/users/<id>')
-def get_user(id):
-   if id :
-      for user in users['users_list']:
-        if user['id'] == id:
-           return user
-      return ({})
+# @app.route('/users/<id>')
+# def get_user(id):
+#    if id :
+#       for user in users['users_list']:
+#         if user['id'] == id:
+#            return user
+#       return ({})
+#    return users
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      search_job = request.args.get('job')
+      if search_username and search_job:
+         return filter_name_and_job(search_username,search_job)
+      elif search_username:
+         return filter_name(search_username)
+      return users  
+
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
+
+    
+
+@app.route("/users/<id>", methods=['GET','DELETE'])
+def get_usersById(id):
+   if id:
+        for user in users['users_list']:
+           if user['id']== id:
+              if request.method =='GET':
+                  return user
+              elif request.method == 'DELETE':
+                  users['users_list'].remove(user)
+                  resp = jsonify(),204
+                  return resp
+        res = jsonify({"could not fund the item given id"}),404
+        return res
    return users
-   
+
+
+def filter_name_and_job(name,job):
+      subdict ={'users_list':[]}
+      for user in users['users_list']:
+         if user['name']== name and user['job']==job:
+            subdict['users_list'].append(user)
+      return subdict
+
+
+def filter_name(name):
+      subdict ={'users_list':[]}
+      #search_username = request.args.get('name') #accessing the value of parameter 'name'
+      for user in users['users_list']:
+         if user['name']== name:
+            subdict['users_list'].append(user)
+      return subdict
+
+
 users = { 
    'users_list' :
    [
